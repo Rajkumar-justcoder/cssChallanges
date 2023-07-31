e79f17f1425a873da13c6cc422a6a35034c5e095
Rajkumar-justcoder <meet.r.graphicat10@gmail.com>
2023-07-31 20:31:20 +0530

    Feat: added day 44 challange

---
+++ src/challanges/day44/day44.js
@@ -0,0 +1,18 @@
+function init(index) {
+    const parent = document.querySelector('.frame');
+    const child = document.createElement('span');
+    child.classList.add('cube');
+    child.style.setProperty('width', `${210 - (index * 10)}px`);
+    child.style.setProperty('height', `${210 - (index * 10)}px`);
+    // child.style.setProperty('left', `${(400 - 210 - (index * 10) / 2)}px`);
+    // child.style.setProperty('top', `${(400 - 210 - (index * 10) / 2)}px`);
+    child.style.setProperty('border-color', `rgb(${70 + (index * 8), 70 + (index * 8), 70 + (index * 8)})`);
+    child.style.setProperty('animation', `rotate 2s ease-in-out ${index / 30}s alternate infinite`);
+// child.style.setProperty('bo')
+
+    parent.appendChild(child);
+}
+
+for (let index = 1; index <= 20; index++) {
+    init(index)
+}
